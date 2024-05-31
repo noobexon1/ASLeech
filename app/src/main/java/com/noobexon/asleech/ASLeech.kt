5aac062447eff13b6283268352388db9c318aeb7
noobexon <noamf92@gmail.com>
2024-05-31 15:44:55 +0300

    refactored to event handler base on AS events and a main class to handle all the rest

--- app/src/main/java/com/noobexon/asleech/ASLeech.kt
+++ app/src/main/java/com/noobexon/asleech/ASLeech.kt
@@ -5,12 +5,20 @@ import android.view.accessibility.AccessibilityEvent
 
 class ASLeech : AccessibilityService() {
 
+    private lateinit var eventHandler: EventHandler
+
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        eventHandler = EventHandler()
+    }
+
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        // Handle accessibility events
+        event?.let {
+            eventHandler.handleEvent(event)
+        }
     }
 
     override fun onInterrupt() {
         // Handle service interruption
     }
-
 }
